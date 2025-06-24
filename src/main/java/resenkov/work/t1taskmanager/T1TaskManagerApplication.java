0a09c0e88261816c608dec7ac7a76c8cfc494915
Роман <129206895+Resenkov@users.noreply.github.com>
2025-06-24 12:56:38 +0300

    Initial commit

---
+++ src/main/java/resenkov/work/t1taskmanager/T1TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package resenkov.work.t1taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1TaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1TaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/resenkov/work/t1taskmanager/T1TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package resenkov.work.t1taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
